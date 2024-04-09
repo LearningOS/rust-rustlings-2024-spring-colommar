cfb95244b79a786481c699a2d03b986969493c85
colommar <yfxx_weiyx@163.com>
2024-04-09 18:34:41 +0800

    Update tests9.rs

--- exercises/tests/tests9.rs
+++ exercises/tests/tests9.rs
@@ -27,10 +27,8 @@
 //
 // You should NOT modify any existing code except for adding two lines of attributes.
 
-// I AM NOT DONE
-
 extern "Rust" {
-    #[link_name="my_demo_function"] fn my_demo_function(a: u32) -> u32;
+    fn my_demo_function(a: u32) -> u32;
     #[link_name="my_demo_function"] fn my_demo_function_alias(a: u32) -> u32;
 }
 
